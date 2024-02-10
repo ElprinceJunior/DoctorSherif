9e79949b0f5e7c3fbc03ca89179416627e57cd2a
Doctor sherif <youssef.yasca.elprince@gmail.com>
2024-02-11 01:39:33 +0200

    first commit

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,44 +1,26 @@
 import React from "react";
 import './footer.css';
-const PDF_FILE_URL = 'http://localhost:3000/cv.pdf'
+const PDF_FILE_URL = 'http://localhost:3000/cv.pdf';
 
 const Footer = () => {
-  const downloadFileAtURL = (url) => {
-    fetch(url)
-      .then(response => response.blob())
-      .then(blob => {
-        const blobURL = window.URL.createObjectURL(new Blob([blob]))
-        const fileName = url.split('/').pop()
-
-        if (isMobileDevice()) {
-          // For mobile devices, open the file in a new tab
-          window.open(blobURL)
-        } else {
-          // For non-mobile devices, trigger the file download
-          const aTag = document.createElement('a')
-          aTag.href = blobURL
-          aTag.setAttribute('download', fileName)
-          document.body.appendChild(aTag)
-          aTag.click()
-          aTag.remove()
-        }
-      })
-  }
-
-  const isMobileDevice = () => {
-    return window.innerWidth <= 768; // Adjust the breakpoint as per your needs
-  }
+  const handleDownloadCV = () => {
+    const link = document.createElement('a');
+    link.href = PDF_FILE_URL;
+    link.download = 'cv.pdf';
+    link.target = '_blank';
+    link.click();
+  };
 
   return (
     <footer className="footer">
-      <button onClick={() => { downloadFileAtURL(PDF_FILE_URL) }}>
+      <button onClick={handleDownloadCV}>
         Download CV
       </button>
       <div>
         copyright &#169; 2024 youssef. All right received.
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
