c04f98e4cbd3f91f255fddfc068d0a3a0864d390
Doctor sherif <youssef.yasca.elprince@gmail.com>
2024-02-11 02:08:08 +0200

    first commit

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import './footer.css';
+
 const PDF_FILE_URL = 'http://localhost:3000/cv.pdf';
 
 const Footer = () => {
+  const downloadRef = useRef(null);
+
   const handleDownloadCV = () => {
+    // Create a hidden <a> element
     const link = document.createElement('a');
     link.href = PDF_FILE_URL;
-    link.download = 'cv.pdf';
     link.target = '_blank';
+    link.download = 'cv.pdf';
+
+    // Append the link to the document body
+    document.body.appendChild(link);
+
+    // Simulate a click event to trigger the download
     link.click();
+
+    // Clean up by removing the link from the document body
+    document.body.removeChild(link);
   };
 
   return (
