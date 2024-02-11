a4ab273e4828c68d9197d89a467c49c08c4e6039
Doctor sherif <youssef.yasca.elprince@gmail.com>
2024-02-11 17:11:22 +0200

    New New

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,41 +1,18 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./footer.css";
 
-const PDF_FILE_URL = "http://localhost:3000/cv.pdf";
+const PDF_FILE_URL = "/cv.pdf"; // Update the PDF file path relative to the public folder
 
 const Footer = () => {
-  const downloadRef = useRef(null);
-
   const handleDownloadCV = () => {
-    // Check if the user is on a mobile device
-    const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
-    );
-
-    if (isMobileDevice) {
-      // Open the PDF file in a new tab
-      window.open(PDF_FILE_URL, "_blank");
-    } else {
-      // Create a hidden <a> element
-      const link = document.createElement("a");
-      link.href = PDF_FILE_URL;
-      link.target = "_blank";
-      link.download = "cv.pdf";
-
-      // Append the link to the document body
-      document.body.appendChild(link);
-
-      // Simulate a click event to trigger the download
-      link.click();
-
-      // Clean up by removing the link from the document body
-      document.body.removeChild(link);
-    }
+    // Open the PDF file in a new tab
+    window.open(PDF_FILE_URL, "_blank");
   };
 
   return (
     <footer className="footer">
       <button onClick={handleDownloadCV}>Download CV</button>
+      <a href="/cv.pdf">download cv</a>
       <div>copyright &#169; 2024 youssef. All right received.</div>
     </footer>
   );
