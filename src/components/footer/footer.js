06b66e4cb761e63e7824578996bace520b4e4803
Doctor sherif <youssef.yasca.elprince@gmail.com>
2024-02-11 02:33:19 +0200

    i have made the cv download

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,36 +1,42 @@
 import React, { useRef } from "react";
-import './footer.css';
+import "./footer.css";
 
-const PDF_FILE_URL = 'http://localhost:3000/cv.pdf';
+const PDF_FILE_URL = "http://localhost:3000/cv.pdf";
 
 const Footer = () => {
   const downloadRef = useRef(null);
 
   const handleDownloadCV = () => {
-    // Create a hidden <a> element
-    const link = document.createElement('a');
-    link.href = PDF_FILE_URL;
-    link.target = '_blank';
-    link.download = 'cv.pdf';
+    // Check if the user is on a mobile device
+    const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
 
-    // Append the link to the document body
-    document.body.appendChild(link);
+    if (isMobileDevice) {
+      // Open the PDF file in a new tab
+      window.open(PDF_FILE_URL, "_blank");
+    } else {
+      // Create a hidden <a> element
+      const link = document.createElement("a");
+      link.href = PDF_FILE_URL;
+      link.target = "_blank";
+      link.download = "cv.pdf";
 
-    // Simulate a click event to trigger the download
-    link.click();
+      // Append the link to the document body
+      document.body.appendChild(link);
 
-    // Clean up by removing the link from the document body
-    document.body.removeChild(link);
+      // Simulate a click event to trigger the download
+      link.click();
+
+      // Clean up by removing the link from the document body
+      document.body.removeChild(link);
+    }
   };
 
   return (
     <footer className="footer">
-      <button onClick={handleDownloadCV}>
-        Download CV
-      </button>
-      <div>
-        copyright &#169; 2024 youssef. All right received.
-      </div>
+      <button onClick={handleDownloadCV}>Download CV</button>
+      <div>copyright &#169; 2024 youssef. All right received.</div>
     </footer>
   );
 };
